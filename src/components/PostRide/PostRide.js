ccf511091655ad7271ee88f60a50714cae6cb01f
Michael J R <michael.rubenstein@mail.utoronto.ca>
2022-09-30 15:06:33 -0400

    added post a ride form and styles

--- src/components/PostRide/PostRide.js
+++ src/components/PostRide/PostRide.js
@@ -1,39 +1,54 @@
 import Modal from "../UI/Modal";
 
+import closeIcon from "../../images/close.svg";
+
 import styles from "./PostRide.module.css";
 
-const PostRide = () => {
+const PostRide = (props) => {
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
+      <img
+        srcSet={closeIcon}
+        onClick={props.onClose}
+        className={styles["close-icon"]}
+      ></img>
       <h2 className={styles.title}>Post a Ride</h2>
       <form>
-        <div className={styles["input-group"]}>
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" />
+        <div className={styles["inputs-group"]}>
+          <div className={styles["input-group"]}>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" />
+          </div>
+          <div className={styles["input-group"]}>
+            <label htmlFor="number">Number</label>
+            <input type="number" id="number" />
+          </div>
         </div>
-        <div className={styles["input-group"]}>
-          <label htmlFor="number">Number</label>
-          <input type="number" id="number" />
-        </div>
-        <div className={styles["input-group"]}>
+        <div
+          className={`${styles["input-group"]} ${styles["email-input-group"]}`}
+        >
           <label htmlFor="email">School Email</label>
           <input type="email" id="email" />
         </div>
-        <div className={styles["input-group"]}>
-          <label htmlFor="dest">Destination</label>
-          <input type="text" id="dest" maxLength={50} />
+        <div className={styles["inputs-group"]}>
+          <div className={styles["input-group"]}>
+            <label htmlFor="dest">Destination</label>
+            <input type="text" id="dest" maxLength={50} />
+          </div>
+          <div className={styles["input-group"]}>
+            <label htmlFor="seats">Available Seats</label>
+            <input type="number" id="seats" step={1} min={1} />
+          </div>
         </div>
-        <div className={styles["input-group"]}>
-          <label htmlFor="seats">Available Seats</label>
-          <input type="number" id="seats" step={1} min={1} />
-        </div>
-        <div className={styles["input-group"]}>
-          <label htmlFor="date">Departure Date</label>
-          <input type="date" id="date" />
-        </div>
-        <div className={styles["input-group"]}>
-          <label htmlFor="time">Departure Time</label>
-          <input type="time" id="time" />
+        <div className={styles["inputs-group"]}>
+          <div className={styles["input-group"]}>
+            <label htmlFor="date">Departure Date</label>
+            <input type="date" id="date" />
+          </div>
+          <div className={styles["input-group"]}>
+            <label htmlFor="time">Departure Time</label>
+            <input type="time" id="time" />
+          </div>
         </div>
         <div className={styles["input-group-checkbox"]}>
           <div>
