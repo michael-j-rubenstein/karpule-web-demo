3f4cc0f61fb61b17d9cd75f763e2e2702f829ea4
Michael J R <michael.rubenstein@mail.utoronto.ca>
2022-09-30 16:33:15 -0400

    connected database to post and read rides

--- src/components/Rides/AvailableRides.js
+++ src/components/Rides/AvailableRides.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import Card from "../UI/Card";
 import RideItem from "./RideItem/RideItem";
 
@@ -30,7 +32,48 @@ const RIDES_DATA = [
 ];
 
 const AvailableRides = (props) => {
-  const rideData = RIDES_DATA.map((data) => {
+  const [rides, setRides] = useState([]);
+
+  useEffect(() => {
+    const fetchRides = async () => {
+      const response = await fetch(
+        "https://karpule-web-demo-default-rtdb.firebaseio.com/rides.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const responseData = await response.json();
+
+      const loadedRides = [];
+
+      for (let key in responseData) {
+        const dateData = await responseData[key].date.split("-");
+        const timeData = await responseData[key].time.split(":");
+        loadedRides.push({
+          key: key,
+          date: new Date(
+            dateData[0],
+            dateData[1] - 1,
+            dateData[2],
+            timeData[0],
+            timeData[1]
+          ),
+          destination: responseData[key].dest,
+          seats: responseData[key].seats,
+          orice: responseData[key].price,
+        });
+        setRides(loadedRides);
+      }
+    };
+
+    fetchRides().catch((error) => {
+      console.log(error.message);
+    });
+  }, []);
+
+  const rideData = rides.map((data) => {
     return (
       <RideItem
         className={styles["rides-item"]}
