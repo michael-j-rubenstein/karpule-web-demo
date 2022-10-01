4e8289d0ff55c107de28c16ac076f83514989ceb
Michael J R <michael.rubenstein@mail.utoronto.ca>
2022-10-01 03:00:00 -0400

    added company logo

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,11 +1,16 @@
 import styles from "./Header.module.css";
 
+import messageIcon from "../../images/message.svg";
+import logo from "../../images/logo.svg";
+
 const Header = () => {
   return (
     <div className={styles["header-wrapper"]}>
       <header className={styles.header}>
-        <h1>Karpule</h1>
-        <button href="#">Contact</button>
+        <img srcSet={logo} alt=""></img>
+        <a href="mailto:babsonkarpule@gmail.com">
+          <img className={styles.svg} srcSet={messageIcon} alt=""></img>
+        </a>
       </header>
       <div className={styles["back-drop-wrapper"]}>
         <div className={styles["back-drop"]}></div>
