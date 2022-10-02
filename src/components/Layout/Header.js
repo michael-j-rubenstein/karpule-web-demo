0cfbddf2586d8737aa9419a570207af5efbc5f0f
Michael J R <michael.rubenstein@mail.utoronto.ca>
2022-10-01 20:31:47 -0400

    fixed overflowing error

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -5,17 +5,19 @@ import logo from "../../images/logo.svg";
 
 const Header = () => {
   return (
-    <div className={styles["header-wrapper"]}>
-      <header className={styles.header}>
-        <img srcSet={logo} alt=""></img>
-        <a href="mailto:babsonkarpule@gmail.com">
-          <img className={styles.svg} srcSet={messageIcon} alt=""></img>
-        </a>
-      </header>
+    <>
+      <div className={styles["header-wrapper"]}>
+        <header className={styles.header}>
+          <img srcSet={logo} alt=""></img>
+          <a href="mailto:babsonkarpule@gmail.com">
+            <img className={styles.svg} srcSet={messageIcon} alt=""></img>
+          </a>
+        </header>
+      </div>
       <div className={styles["back-drop-wrapper"]}>
         <div className={styles["back-drop"]}></div>
       </div>
-    </div>
+    </>
   );
 };
 
