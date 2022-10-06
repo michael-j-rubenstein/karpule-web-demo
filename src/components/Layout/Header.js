c80fa482e68beba5719dc839ef7d64ac333add12
Michael J R <michael.rubenstein@mail.utoronto.ca>
2022-10-06 13:03:14 -0400

    added instagram link

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,12 +2,20 @@ import styles from "./Header.module.css";
 
 import messageIcon from "../../images/message.svg";
 import logo from "../../images/logo.svg";
+import ins from "../../images/instagram-logo.svg";
 
 const Header = () => {
   return (
     <>
       <div className={styles["header-wrapper"]}>
         <header className={styles.header}>
+          <a
+            href="https://www.instagram.com/karpulego/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className={styles.ins} srcSet={ins} alt=""></img>
+          </a>
           <img srcSet={logo} alt=""></img>
           <a href="mailto:babsonkarpule@gmail.com">
             <img className={styles.svg} srcSet={messageIcon} alt=""></img>
